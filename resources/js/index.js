35e75a82a83db3873a91411723960c40992d151d
mzaini30 <muhzaini30@gmail.com>
2024-06-07 13:11:40 +0800

    upload

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,5 @@
-import * as L from 'leaflet';
-import 'leaflet-fullscreen';
+import * as L from "leaflet";
+import "leaflet-fullscreen";
 
 window.dekaMapPicker = ($wire, config) => {
     return {
@@ -8,16 +8,15 @@ window.dekaMapPicker = ($wire, config) => {
         marker: null,
         polygon: [],
         createMap: function (el) {
-
             const that = this;
 
             this.map = L.map(el, config.controls);
-            this.map.on('load', () => {
+            this.map.on("load", () => {
                 setTimeout(() => this.map.invalidateSize(true), 0);
                 if (config.showMarker === true) {
-                    this.marker.setLatLng(this.map.getCenter())
+                    this.marker.setLatLng(this.map.getCenter());
                 }
-            })
+            });
 
             if (!config.draggable) {
                 this.map.dragging.disable();
@@ -32,7 +31,6 @@ window.dekaMapPicker = ($wire, config) => {
                 detectRetina: config.detectRetina,
             }).addTo(this.map);
 
-
             if (config.showMarker === true) {
                 const markerColor = config.markerColor || "#3b82f6";
                 const svgIcon = L.divIcon({
@@ -44,34 +42,46 @@ window.dekaMapPicker = ($wire, config) => {
                 this.marker = L.marker([0, 0], {
                     icon: svgIcon,
                     draggable: false,
-                    autoPan: true
-                }).addTo(this.map)
-                this.map.on('move', () => this.marker.setLatLng(this.map.getCenter()))
+                    autoPan: true,
+                }).addTo(this.map);
+                this.map.on("move", () =>
+                    this.marker.setLatLng(this.map.getCenter()),
+                );
             }
 
-            this.map.on('moveend', () => {
+            this.map.on("moveend", () => {
                 let coordinates = this.getCoordinates();
-                if (config.draggable && (coordinates.lng !== this.map.getCenter()['lng'] || coordinates.lat !== this.map.getCenter()['lat'])) {
+                if (
+                    config.draggable &&
+                    (coordinates.lng !== this.map.getCenter()["lng"] ||
+                        coordinates.lat !== this.map.getCenter()["lat"])
+                ) {
                     // $wire.set(config.statePath, this.map.getCenter(), false)
 
-                    console.log(config.statePath)
-                    console.log('moveend')
-                    $wire.set(config.statePath, {
-                        lat: this.map.getCenter().lat,
-                        lng: this.map.getCenter().lng,
-                        polygon: $wire.get(config.statePath).polygon ?? this.polygon
-                    }, false)
-                    console.log($wire.get(config.statePath))
+                    console.log(config.statePath);
+                    console.log("moveend");
+                    $wire.set(
+                        config.statePath,
+                        {
+                            lat: this.map.getCenter().lat,
+                            lng: this.map.getCenter().lng,
+                            polygon:
+                                $wire.get(config.statePath).polygon ??
+                                this.polygon,
+                        },
+                        false,
+                    );
+                    console.log($wire.get(config.statePath));
                     // di sini berhasil
 
-                    if(config.liveLocation){
+                    if (config.liveLocation) {
                         $wire.$refresh();
                     }
                 }
-            })
+            });
 
-            this.map.on('locationfound', function () {
-                that.map.setZoom(config.controls.zoom)
+            this.map.on("locationfound", function () {
+                that.map.setZoom(config.controls.zoom);
             });
             let location = this.getCoordinates();
             if (!location.lat && !location.lng) {
@@ -79,165 +89,169 @@ window.dekaMapPicker = ($wire, config) => {
                     setView: true,
                     maxZoom: config.controls.maxZoom,
                     enableHighAccuracy: true,
-                    watch: false
+                    watch: false,
                 });
             } else {
-                this.map.setView(new L.LatLng(location.lat, location.lng))
+                this.map.setView(new L.LatLng(location.lat, location.lng));
             }
 
             // menggambar polygon
             //
 
             const createAreaTooltip = (layer) => {
-                if(layer.areaTooltip) {
+                if (layer.areaTooltip) {
                     return;
                 }
 
                 layer.areaTooltip = L.tooltip({
                     permanent: true,
-                    direction: 'center',
-                    className: 'area-tooltip'
+                    direction: "center",
+                    className: "area-tooltip",
                 });
 
-                layer.on('remove', function(event) {
+                layer.on("remove", function (event) {
                     layer.areaTooltip.remove();
                 });
 
-                layer.on('add', (event) => {
+                layer.on("add", (event) => {
                     updateAreaTooltip(layer);
                     layer.areaTooltip.addTo(this.map);
                 });
 
-                if(this.map.hasLayer(layer)) {
+                if (this.map.hasLayer(layer)) {
                     updateAreaTooltip(layer);
                     layer.areaTooltip.addTo(this.map);
                 }
-            }
+            };
 
             const updateAreaTooltip = (layer) => {
-                var area = window.L.GeometryUtil.geodesicArea(layer.getLatLngs()[0]);
-                var readableArea = window.L.GeometryUtil.readableArea(area, true);
+                var area = window.L.GeometryUtil.geodesicArea(
+                    layer.getLatLngs()[0],
+                );
+                var readableArea = window.L.GeometryUtil.readableArea(
+                    area,
+                    true,
+                );
                 var latlng = layer.getCenter();
 
-                layer.areaTooltip
-                    .setContent(readableArea)
-                    .setLatLng(latlng);
+                layer.areaTooltip.setContent(readableArea).setLatLng(latlng);
+            };
+
+            console.log("Cek state");
+            console.log($wire.get(config.statePath));
+
+            console.log("Cek polygon");
+            console.log($wire.get(config.statePath).polygon);
+
+            let polygonMauCetak = [];
+            for (let koordinat of $wire.get(config.statePath).polygon) {
+                polygonMauCetak.push([koordinat.lat, koordinat.lng]);
             }
 
-            console.log('Cek state')
-            console.log($wire.get(config.statePath))
-
-            console.log('Cek polygon')
-            console.log($wire.get(config.statePath).polygon)
-
-            let polygonMauCetak = []
-            for (let koordinat of $wire.get(config.statePath).polygon){
-                polygonMauCetak.push([koordinat.lat, koordinat.lng])
-            }
-
-            let polygon = L.polygon(
-                polygonMauCetak
-            ).addTo(this.map);
+            let polygon = L.polygon(polygonMauCetak).addTo(this.map);
 
             createAreaTooltip(polygon);
 
-
             /**
-            * EXAMPLE WITH LEAFLET DRAW CONTROL
-            */
+             * EXAMPLE WITH LEAFLET DRAW CONTROL
+             */
             var drawnItems = L.featureGroup().addTo(this.map);
 
-            this.map.addControl(new L.Control.Draw({
-                edit: {
-                    featureGroup: drawnItems,
-                    poly: {
-                        allowIntersection: false
-                    }
-                },
-                draw: {
-                    marker: false,
-                    circle: false,
-                    circlemarker: false,
-                    rectangle: false,
-                    polyline: false,
-                    polygon: {
-                        allowIntersection: false,
-                        showArea: true
-                    }
-                }
-            }));
-
-
+            this.map.addControl(
+                new L.Control.Draw({
+                    edit: {
+                        featureGroup: drawnItems,
+                        poly: {
+                            allowIntersection: false,
+                        },
+                    },
+                    draw: {
+                        marker: false,
+                        circle: false,
+                        circlemarker: false,
+                        rectangle: false,
+                        polyline: false,
+                        polygon: {
+                            allowIntersection: false,
+                            showArea: true,
+                        },
+                    },
+                }),
+            );
 
             // console.log(L.Draw)
             this.map.on(window.L.Draw.Event.CREATED, (event) => {
                 var layer = event.layer;
 
-                if(layer instanceof L.Polygon) {
+                if (layer instanceof L.Polygon) {
                     createAreaTooltip(layer);
                 }
 
                 drawnItems.addLayer(layer);
 
-                this.polygon = event.layer['_latlngs'][0]
+                this.polygon = event.layer["_latlngs"][0];
 
                 // $wire.set(config.statePath.polygon, event.layer['_latlngs'])
-                $wire.set(config.statePath, {
-                    lat: config.statePath.lat,
-                    lng: config.statePath.lng,
-                    polygon: this.polygon
-                }, false)
-                console.log($wire.get(config.statePath))
+                $wire.set(
+                    config.statePath,
+                    {
+                        lat: config.statePath.lat,
+                        lng: config.statePath.lng,
+                        polygon: this.polygon,
+                    },
+                    false,
+                );
+                console.log($wire.get(config.statePath));
                 $wire.$refresh();
             });
 
-            this.map.on(window.L.Draw.Event.EDITED, function(event) {
-                event.layers.getLayers().forEach(function(layer) {
-                    if(layer instanceof L.Polygon) {
+            this.map.on(window.L.Draw.Event.EDITED, function (event) {
+                event.layers.getLayers().forEach(function (layer) {
+                    if (layer instanceof L.Polygon) {
                         updateAreaTooltip(layer);
                     }
-                })
+                });
             });
         },
         removeMap: function (el) {
-
             if (this.marker) {
                 this.marker.remove();
-                this.marker = null
+                this.marker = null;
             }
             this.tile.remove();
-            this.tile = null
-            this.map.off()
+            this.tile = null;
+            this.map.off();
             this.map.remove();
-            this.map = null
+            this.map = null;
         },
         getCoordinates: function () {
-            let location = $wire.get(config.statePath)
-            if (location === null || !location.hasOwnProperty('lat')) {
-                location = {lat: 0, lng: 0, polygon: []}
+            let location = $wire.get(config.statePath);
+            if (location === null || !location.hasOwnProperty("lat")) {
+                location = { lat: 0, lng: 0, polygon: [] };
             }
 
             return location;
         },
         attach: function (el) {
-
-            this.createMap(el)
-            const observer = new IntersectionObserver(entries => {
-                entries.forEach(entry => {
-                    if (entry.intersectionRatio > 0) {
-                        if (!this.map)
-                            this.createMap(el)
-                    } else {
-                        this.removeMap(el)
-                    }
-                });
-            }, {
-                root: null, // set document viewport as root
-                rootMargin: '0px', // margin around root
-                threshold: 1.0 // 1.0 means that when 100% of the target is visible
-            });
+            this.createMap(el);
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.intersectionRatio > 0) {
+                            if (!this.map) this.createMap(el);
+                        } else {
+                            this.removeMap(el);
+                        }
+                    });
+                },
+                {
+                    root: null, // set document viewport as root
+                    rootMargin: "0px", // margin around root
+                    threshold: 1.0, // 1.0 means that when 100% of the target is visible
+                },
+            );
             observer.observe(el);
-        }
-    }
-}
-window.dispatchEvent(new CustomEvent('map-script-loaded'));
+        },
+    };
+};
+window.dispatchEvent(new CustomEvent("map-script-loaded"));
