945dfc63656312b981f60593cba38fffd3ad767b
mzaini30 <muhzaini30@gmail.com>
2024-06-07 13:14:59 +0800

    upload

--- resources/views/fields/osm-map-picker.blade.php
+++ resources/views/fields/osm-map-picker.blade.php
@@ -3,8 +3,8 @@
     <div x-data="async () => {
         do {
             await (new Promise(resolve => setTimeout(resolve, 100)));
-        } while (window.mapPicker === undefined);
-        const m = mapPicker($wire, {{ $getMapConfig() }});
+        } while (window.dekaMapPicker === undefined);
+        const m = dekaMapPicker($wire, {{ $getMapConfig() }});
         m.attach($refs.map);
     }" wire:ignore>
         <div x-ref="map" class="w-full" style="min-height: 30vh; z-index: 1 !important;">
